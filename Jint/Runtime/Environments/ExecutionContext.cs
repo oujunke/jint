11f72530e85c4579b16d7737a794c82e971629c4
oujunke <oujunke@dfl.com.cn>
2021-04-15 09:50:01 +0800

    添加TryGetBinding方法

--- Jint/Runtime/Environments/ExecutionContext.cs
+++ Jint/Runtime/Environments/ExecutionContext.cs
@@ -1,4 +1,6 @@
-﻿namespace Jint.Runtime.Environments
+﻿using Jint.Native;
+
+namespace Jint.Runtime.Environments
 {
     public readonly struct ExecutionContext
     {
@@ -21,6 +23,43 @@
         public ExecutionContext UpdateVariableEnvironment(LexicalEnvironment variableEnvironment)
         {
             return new ExecutionContext(LexicalEnvironment, variableEnvironment);
+        }
+        /// <summary>
+        /// 获得变量
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="strict"></param>
+        /// <param name="record"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetBinding(string name,
+            bool strict,
+            out EnvironmentRecord record,
+            out JsValue value)
+        {
+            EnvironmentRecord.BindingName bindingName = new EnvironmentRecord.BindingName(name);
+            if (LexicalEnvironment.TryGetIdentifierEnvironmentWithBindingValue(LexicalEnvironment, in bindingName, strict, out record, out value))
+            {
+                return true;
+            }
+            else if (LexicalEnvironment.TryGetIdentifierEnvironmentWithBindingValue(VariableEnvironment, in bindingName, strict, out record, out value))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 获得变量
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="strict"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetBinding(string name,
+           bool strict,
+           out JsValue value)
+        {
+            return TryGetBinding(name,strict,out _,out value);
         }
     }
 }
